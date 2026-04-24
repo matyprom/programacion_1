2d54d4117e3426ea123343ab360cf8e07847016b
matyprom <you@example.com>
2026-04-24 12:14:14 -0300

    3

--- clase 7/ejer3.py
+++ clase 7/ejer3.py
@@ -1,12 +1,12 @@
 numeros = [-1, 1, -2, -3, 7, 10]
-numero_negativos=[]
-numero_positivos=[]
+numero_negativos=0
+numero_positivos=0
 suma=0
 for i in numeros:
     if i>0:
-        numero_positivos.append(i)
+        numero_positivos+=1
     else:
-        numero_negativos.append(i)
+        numero_negativos+=1
     suma+=i
 print("suma total: ", suma)
 print("numero negativos: ", numero_negativos)
