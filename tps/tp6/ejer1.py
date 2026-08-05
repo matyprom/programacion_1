5f7aa49a7d32aee52cd70df1aa244e879a5d1d87
matyprom <you@example.com>
2026-08-05 20:03:00 -0300

    3

--- tps/tp6/ejer1.py
+++ tps/tp6/ejer1.py
@@ -1,5 +1,5 @@
-ciudades = set()
-fechas = set()
+ciudades = []
+fechas = []
 registros = [
     ("2026-04-07", "Bariloche", 18),
     ("2026-04-07", "Viedma", 31),
@@ -11,16 +11,16 @@ registros = [
     ("2026-04-21", "Viedma", 27),
     ("2026-04-21", "El Bolson", 19)
 ]
-registros= set(registros)
-
-for registros in registros:
-    ciudad = registros[1]
-    varfechas= registros[0]
-    ciudades.add(ciudad)
-    fechas.add(varfechas)
-
-
-print(fechas)
-print(ciudades)
-
-#uh ni idea estoy re perdido
+promedio_ciudad={}
+for fecha, ciudad, temp in registros:
+    if ciudad not in promedio_ciudad:
+        promedio_ciudad[ciudad]=[]
+    promedio_ciudad[ciudad].append(temp)
+    ciudades.append(ciudad)
+    fechas.append(fecha)
+for ciudad in promedio_ciudad:
+    tem=promedio_ciudad[ciudad]
+    promedio_ciudad[ciudad]= sum(tem)/len(tem)
+print(set(ciudades))
+print(set(fechas))
+print(promedio_ciudad)
